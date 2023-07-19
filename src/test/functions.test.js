36fa00b25b4188038f54a8ed479f4ff57f4e3534
kambereBr <brunokambere@gmail.com>
2023-07-19 22:55:48 +0200

    Configure Jest, add and test function stringLength

---
+++ src/test/functions.test.js
@@ -0,0 +1,23 @@
+const { expect } = require('@jest/globals');
+const stringLength = require('../functions')
+
+test('String "Hello!" returns character count 6', () => {
+    expect(stringLength('Hello!')).toBe(6);
+})
+
+test('A string with character count greather 10 throws an error', () => {
+    expect(() => stringLength('Hello World!')).toThrow();
+})
+
+test('An empty string throws an error', () => {
+    expect(() => stringLength('')).toThrow();
+})
+
+test('All data type other than numerics throws an error', () => {
+    expect(() => stringLength(1)).toThrow();
+    expect(() => stringLength(null)).toThrow();
+    expect(() => stringLength()).toThrow();
+    expect(() => stringLength(abc)).toThrow();
+    expect(() => stringLength({a: 'test'})).toThrow();
+    expect(() => stringLength([1, 'test', 2])).toThrow();
+})
